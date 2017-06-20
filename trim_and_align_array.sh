2b580a2b197b76f8045a28d0659c97e95bcfa29b
boberrey <bober.reynolds@gmail.com>
2017-06-20 15:21:15 -0700

    Changed the names of the 'filter_fastqs*' scripts to be more specific

--- trim_and_align_array.sh
+++ trim_and_align_array.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 # get_stranded_fastas.sh
 
-### bash script for trimming and aligning reads. Output is a name-sorted bam file.
+### bash script for trimming and aligning reads for array applications. 
+### For array applications, we do not want to remove PCR duplicates, and we 
+### are interested in getting the exact sequences that appear in the 
+### experiment (i.e. strand matters). This script will output 
+
+### This script assumes you are starting with fastqs. If you are starting from 
+### array-format data (i.e. CPseq), you can filter the original fastqs using 
+### a list of clusters that are relevant to your experiment (which can 
+### be obtained from the CPseq file format)
+
 ### Use like so:
 
 # ./trim_and_align.sh r1.fastq r2.fastq adapters.fa ref_genome output_dir output_prefix
@@ -10,10 +19,10 @@
 
 # Check that the proper number of parameters were given
 if [ "$#" -ne 6 ]; then
-	echo "	bash script for getting insert sequence fasta files from paired end reads."
-	echo "	Output is a coordinate-sorted bam file."
+	echo "	bash script for aligning fastqs and getting relevant files for array applications"
+	echo "	Output is a name-sorted bam file (for generating 'insert' bed files and fasta files)"
 	echo "	Usage:"
-	echo "	trim_and_align.sh r1.fastq r2.fastq adapters.fa ref_genome output_dir output_prefix"
+	echo "	trim_and_align_array.sh r1.fastq r2.fastq adapters.fa ref_genome output_dir output_prefix"
 	exit
 fi
 
@@ -35,8 +44,6 @@ tr2="$output_prefix-trimmed-pair2.fastq"
 trim_logfile="$output_prefix-trimmed.log"
 sam_file="$output_dir$output_prefix.sam"
 unsorted_bam_file="$output_dir$output_prefix-unsorted.bam"
-coordinate_sorted_bam="$output_dir$output_prefix-c-sorted.bam"
-dups_removed_bam="$output_dir$output_prefix-no_dups.bam"
 name_sorted_bam="$output_dir$output_prefix.bam"
 metrics_file="$output_dir$output_prefix-metrics"
 
@@ -58,27 +65,14 @@ samtools view -b -S -o $unsorted_bam_file $sam_file 2>&1
 
 # sort and clean up
 echo ""
-echo "Sorting and removing duplicates..."
+echo "Sorting by name for bedpe creation..."
 echo ""
-
-# sort by coordinate (required for removing duplicates)
-# Redirect picard metrics (normally stderr) to stdout for better wrapper script handling:
-java -jar -XX:ParallelGCThreads=$n_cores \
--Djava.io.tmpdir=`pwd`/tmp /app/picard/picard-tools-1.130/picard.jar \
-SortSam SO=coordinate I=$unsorted_bam_file O=$coordinate_sorted_bam \
-VALIDATION_STRINGENCY=SILENT 2>&1
-
-# Remove PCR duplicates:
-java -jar -XX:ParallelGCThreads=$n_cores \
--Djava.io.tmpdir=`pwd`/tmp /app/picard/picard-tools-1.130/picard.jar \
-MarkDuplicates INPUT=$coordinate_sorted_bam OUTPUT=$dups_removed_bam \
-METRICS_FILE=$metrics_file REMOVE_DUPLICATES=true VALIDATION_STRINGENCY=SILENT 2>&1
 
 # sort by name (required to make bedpe)
 java -jar -XX:ParallelGCThreads=$n_cores \
 -Djava.io.tmpdir=`pwd`/tmp /app/picard/picard-tools-1.130/picard.jar \
-SortSam SO=queryname I=$dups_removed_bam O=$name_sorted_bam \
+SortSam SO=queryname I=$unsorted_bam_file O=$name_sorted_bam \
 VALIDATION_STRINGENCY=SILENT 2>&1
 
 # clean up intermediate files:
-rm $tr1 $tr2 $trim_logfile $sam_file $unsorted_bam_file $coordinate_sorted_bam $dups_removed_bam
+rm $tr1 $tr2 $trim_logfile $sam_file $unsorted_bam_file
